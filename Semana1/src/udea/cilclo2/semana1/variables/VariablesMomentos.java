8d08c57e08ae610bfdf5e0a334a5a07c29d982f8
Carlos Romero <carlos.romero@one-click.com.co>
2022-06-15 13:10:26 -0500

    feat: Semana uno Sesión 1 : Variables, momentos en el ciclo de vida del programa.

---
+++ Semana1/src/udea/cilclo2/semana1/variables/VariablesMomentos.java
@@ -0,0 +1,23 @@
+package udea.cilclo2.semana1.variables;
+
+public class VariablesMomentos {
+
+    /*
+    Variables: Momento 1 Declaración
+               Momento 2 Inicialización
+               Momento 3 Uso 
+     */
+    public static void main(String[] args) {
+
+        //Declaración
+        String nombre;
+
+        // Inicialización
+        nombre = "Carlos ";
+
+        // Uso
+        System.err.println("Mi nombre es  : " + nombre);
+
+    }
+
+}
